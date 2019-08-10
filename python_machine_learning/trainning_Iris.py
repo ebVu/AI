83d82fac47c6e133aa1089f7517eca4a84bb42f7
vunhatphan <pnvupfiev08@gmail.com>
2019-08-10 11:26:30 +0700

    update comment for chapter 2

--- python_machine_learning/trainning_Iris.py
+++ python_machine_learning/trainning_Iris.py
@@ -13,6 +13,7 @@ class Perceptron(object):
         Learning rate (between 0.0 and 1.0)
     n_iter : int
         Passes over the training dataset.
+        So lan lap lai cua tap cac sample data, muc tieu de chay den khi nao hoi tu
 
     Attributes
     -----------
@@ -45,11 +46,16 @@ class Perceptron(object):
         self.w_ = np.zeros(1 + X.shape[1])
         self.errors_ = []
 
+        #chay vong lap 10 lan huan luyen tap mau
         for _ in range(self.n_iter):
             errors = 0
-           # i = 0
+            # i = 0
+            #vong lap cho moi sample
             for xi, target in zip(X, y):
                 update = self.eta * (target - self.predict(xi))
+                # moi trong so duoc update bang cach nhan voi xi nen W_ se 
+                # khac nhau tuy theo moi feature
+                # bien update la 1 scalar
                 self.w_[1:] += update * xi
                 self.w_[0] += update
                 errors += int(update != 0.0) # if stil need update, error is add 1
@@ -66,13 +72,25 @@ class Perceptron(object):
         """Return class label after unit step"""
         return np.where(self.net_input(X) >= 0.0, 1, -1)
 
+#lay data cho y
 y = df.iloc[0:100, 4].values
+
+# chuan hoa lai output
 y = np.where(y == 'Iris-setosa', -1, 1)
+
+# lay data cho X
 X = df.iloc[0:100, [0,2]].values
+
+# X[:50, 0] => bieu dien cho truc sepal length, 50 sample dau la Iris-setosa dc danh mau do
+# X[:50,1] => bieu dien cho truc petal length, 50 sample dau la Iris-setosa dc danh mau do
 plt.scatter(X[:50, 0], X[:50,1], color='red', marker='o', label='setosa')
+
+# X[50:100, 0] => bieu dien cho truc sepal length, 50 sample dau la Iris-setosa dc danh mau do
+# X[50:100,1] => bieu dien cho truc petal length, 50 sample dau la Iris-setosa dc danh mau do
 plt.scatter(X[50:100, 0], X[50:100,1], color='blue', marker='x', label='versicolor')
 plt.xlabel('sepal length')
 plt.ylabel('petal length')
+# legend de o tren cung ben trai
 plt.legend(loc='upper left')
 plt.show()
 
@@ -89,6 +107,21 @@ def plot_decision_regions(X,y, classifier, resolution=0.02):
     markers = ('s', 'x', 'o', '^', 'v')
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
     cmap = ListedColormap(colors[:len(np.unique(y))])
-    
-    
+    x1_min, x1_max = X[:, 0].min()
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+    np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+    # plot class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+            alpha=0.8, c=cmap(idx),
+            marker=markers[idx], label=cl)
+            
     
